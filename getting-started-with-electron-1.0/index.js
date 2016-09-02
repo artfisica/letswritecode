9a7631441014cc70fa5a19587a8da489487c769d
Arturos Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2016-09-02 19:19:03 +0200

    local tries to get the app run. tests

--- getting-started-with-electron-1.0/index.js
+++ getting-started-with-electron-1.0/index.js
@@ -2,9 +2,26 @@
 const remote = require('electron').remote
 const main = remote.require('./main.js')
 
+
+let buttonwebsite = document.createElement('button')
+buttonwebsite.textContent = 'Open ATLAS Open Data website'
+buttonwebsite.addEventListener('click', () => {
+  main.openWeb()
+}, false)
+document.body.appendChild(buttonwebsite)
+
+
 var button = document.createElement('button')
 button.textContent = 'Open Window'
 button.addEventListener('click', () => {
   main.openWindow()
 }, false)
 document.body.appendChild(button)
+
+
+let buttonbigpanda = document.createElement('button')
+buttonbigpanda.textContent = 'Open BigPanda'
+buttonbigpanda.addEventListener('click', () => {
+  main.openBigPanda()
+}, false)
+document.body.appendChild(buttonbigpanda)
--- getting-started-with-electron-1.0/main.js
+++ getting-started-with-electron-1.0/main.js
@@ -11,3 +11,13 @@ exports.openWindow = () => {
   let win = new BrowserWindow({width:400, height: 200})
   win.loadURL(`file://${__dirname}/bear.html`)
 }
+
+exports.openWeb = () => {
+  let AODweb = new BrowserWindow({width:1200, height: 800, frame: true})
+  AODweb.loadURL(`http://opendata.atlas.cern`)
+}
+
+exports.openBigPanda = () => {
+  let Pandaweb = new BrowserWindow({width:1200, height: 800})
+  Pandaweb.loadURL(`http://bigpanda.cern.ch/user/arturos`)
+}
